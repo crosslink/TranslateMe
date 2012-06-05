ffebd362ac8f556877aeaad96d0a83040a07f83a
CN Sturgeon <cnsturgeon2000@yahoo.com>
2012-06-05 16:02:56 +1000

    initial work the translation project

---
+++ src/org/cambia/translate/client/UI.java
@@ -0,0 +1,11 @@
+package org.cambia.translate.client;
+
+import com.google.gwt.user.client.ui.ListBox;
+
+public class UI {
+	public String[] supportedLangs = {""};
+	
+	private ListBox lbLanguages = new ListBox();
+	
+	
+}
