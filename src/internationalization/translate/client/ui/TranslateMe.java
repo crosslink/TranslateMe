5714ea4e2ca0109715aa433795930d985dfb659f
Eric Tang <lingxiang.tang@gmail.com>
2012-07-19 13:37:19 +1000

    I should upload it Github

--- src/internationalization/translate/client/ui/TranslateMe.java
+++ src/internationalization/translate/client/ui/TranslateMe.java
@@ -4,11 +4,14 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.ListBox;
 
 public class TranslateMe extends Composite {
 
 	private static TranslateMeUiBinder uiBinder = GWT
 			.create(TranslateMeUiBinder.class);
+	@UiField ListBox lbLangs;
 
 	interface TranslateMeUiBinder extends UiBinder<Widget, TranslateMe> {
 	}
